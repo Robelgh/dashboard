4cd15e23402a5ae39d10f379be5fc584caf30314
w3schools-test <robelghw12@gmail.com>
2022-08-15 01:04:20 +0300

    first finalize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import {
+  BrowserRouter as Router,
+  Routes ,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import Home from "./components/home";
+import Test1 from "./components/test1";
+import Test2 from "./components/test2";
+import Test3 from "./components/test3";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "React",
+      isUserAuthenticated: true
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/test1">Test 1</Link>
+              </li>
+              <li>
+                <Link to="/test2">Test 2</Link>
+              </li>
+              <li>
+                <Link to="/test3">Test 3</Link>
+              </li>
+            </ul>
+            <hr />
+            <Routes >
+             
+               <Route exact path="/home" component={Home} />
+              <Route exact path="/test1" component={Test1} />
+              <Route exact path="/test2" component={Test2} />
+              <Route exact path="/test3" component={Test3} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/test3.jsx
@@ -0,0 +1,2 @@
+import React from 'react';
+export default Test3 => <div>This is Test3 component</div>;
